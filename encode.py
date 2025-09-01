4c53ba43de2380074b0cec48e8a1354d004bc951
EthanJantz <21095848+EthanJantz@users.noreply.github.com>
2025-08-31 23:40:57 -0500

    init commit

---
+++ encode.py
@@ -0,0 +1,20 @@
+# The puzzle
+def test_encode(x):
+    y = encode(x)
+    assert y[::2] == y[::-2] == x
+
+
+# Define encode
+def encode(x):
+    return n
+
+
+# Define decode
+def decode(x):
+    pass
+
+
+# One of (what I assume is) the encoded strings
+s = "HGTRTOPOMNDDNMOPOTRTGH"
+
+test_encode(s)
